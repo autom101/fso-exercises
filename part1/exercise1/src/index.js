649269c756336c3cc88a816375b6ec4cceb42f0a
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-11 21:34:19 -0600

    Finish ex1.12 and 1.13

--- part1/exercise1/src/index.js
+++ part1/exercise1/src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Unicafe from "./Unicafe";
+import Anecdotes from "./Anecdotes";
 
 // Exercises 1.1-1.5
-//ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+/* 
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+*/
 
-//Exercises 1.6-
+// Exercises 1.6-1.11
+/* 
 ReactDOM.createRoot(document.getElementById("root")).render(<Unicafe />);
+*/
+
+// Exercises 1.12-1.1
+ReactDOM.createRoot(document.getElementById("root")).render(<Anecdotes />);

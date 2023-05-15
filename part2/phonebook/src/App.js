12d2d7839aacccf989af8a9c4e1890250550ea07
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-14 20:48:10 -0600

    Complete ex 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter.js";
 import Person from "./components/Person.js";
 import PersonForm from "./components/PersonForm.js";
@@ -10,6 +11,17 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();

862d0a07ea740ff19991ed53e4f5b71ec6b171df
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-22 21:49:55 -0600

    Add validation and validation messages for Person schema

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -16,13 +16,21 @@ const personsSchema = new mongoose.Schema({
   id: Number,
   name: {
     type: String,
-    minLength: 3,
-    required: true,
+    minLength: [3, "The name must be at least 3 characters long!"],
+    required: [true, "Please enter a name value!"],
   },
   number: {
     type: String,
-    minLength: 1,
-    required: true,
+    minLength: [8, "The phone numbers must be at least 8 characters long!"],
+    validate: {
+      validator: (v) => {
+        return /\d{2,3}-\d+/.test(4);
+      },
+      message: (props) => {
+        `${props.value} is not in the correct format! Sample valid format: 09-1234556 OR 040-22334455`;
+      },
+    },
+    required: [true, "Please enter a phone number"],
   },
 });
 

e00b1a7068015f5f8f0f07e84851116c9dfb54a7
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-22 22:02:52 -0600

    Change json response in errorHandler for ValidationError

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -126,8 +126,11 @@ const errorHandler = (error, request, response, next) => {
   console.log("Error : ", error.message);
   if (error.name === "CastError") {
     response.status(400).send({ error: "Malformatted Id" });
-  } else if (error.name === "ValidationError") {
-    response.status(400).json({ error: error.response.data.error });
+  }
+
+  if (error.name === "ValidationError") {
+    const errorMessage = error.message;
+    response.status(400).json({ error: errorMessage });
   }
 
   next(error);

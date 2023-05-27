2675d85d6e59b5ec3be475dbf264ac71b85870e7
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-26 20:38:41 -0600

    Correct config.js path in index.js

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const config = require("./utils.config");
+const config = require("./utils/config");
 const logger = require("./utils/logger");
 
 const PORT = process.env.PORT || 3003;

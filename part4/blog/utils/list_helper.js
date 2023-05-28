488e8d89ff900af5b0a877a74ff7616463adca9f
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-27 21:04:15 -0600

    Complete ex 4.6

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -10,4 +10,47 @@ const totalLikes = (blogs) => {
   return sum;
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((mostLiked, current) => {
+    if (mostLiked.likes < current.likes) {
+      return current;
+    }
+    return mostLiked;
+  }, blogs[0]);
+
+  const { title, author, likes } = favorite;
+
+  return { title, author, likes };
+};
+
+const mostBlogs = (givenBlogs) => {
+  let authors = {};
+  let highestBlogCount = 1;
+  let authorWithMostBlogs = givenBlogs[0];
+
+  for (let blog of givenBlogs) {
+    const author = blog.author;
+    // If author is already encountered, add to their count. Otherwise add them to authors object
+    if (authors.hasOwnProperty(author)) {
+      authors[author].count = authors[author].count + 1;
+    } else {
+      authors[author] = blog;
+      authors[author].count = 1;
+    }
+
+    const authorBlogCount = authors[author].count;
+    if (authorBlogCount > highestBlogCount) {
+      authorWithMostBlogs = blog;
+      highestBlogCount = authorBlogCount;
+    }
+  }
+
+  const authorToReturn = {
+    author: authorWithMostBlogs.author,
+    blogs: highestBlogCount,
+  };
+
+  return authorToReturn;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };

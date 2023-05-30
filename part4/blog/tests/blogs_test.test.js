fe7119fd7c36ce9cc304c1bdc80b885bd2b7be4a
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-30 15:44:00 -0600

    Complete ex 4.8

--- part4/blog/tests/blogs_test.test.js
+++ part4/blog/tests/blogs_test.test.js
@@ -11,19 +11,24 @@ beforeEach(async () => {
   await Blog.deleteMany({});
 
   const blogPromiseArray = helper.initialBlogs
-    .map((blog) => {
-      blog = new Blog(blog);
-    })
+    .map((blog) => new Blog(blog))
     .map((blog) => blog.save());
 
   await Promise.all(blogPromiseArray);
+}, 20000);
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
 });
 
 test("/api/blogs returns correct amount of blogs", async () => {
   const response = await api.get("/api/blogs");
 
   expect(response.body).toHaveLength(helper.initialBlogs.length);
-});
+}, 100000);
 
 afterAll(async () => {
   await mongoose.connection.close();

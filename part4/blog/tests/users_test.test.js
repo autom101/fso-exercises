49f687217a419268e08e4ea7613db8fade64ef1e
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-06-07 16:03:45 -0600

    Successfully create and pass test that checked that username uniqueness is being verified

--- part4/blog/tests/users_test.test.js
+++ part4/blog/tests/users_test.test.js
@@ -21,35 +21,47 @@ beforeEach(async () => {
   await newUser.save();
 });
 
-test("Successfully create one user", async () => {
-  const usersBefore = await helper.findUsersInDatabase();
+describe("tests with one user in the database", () => {
+  test("Successfully create one user", async () => {
+    const usersBefore = await helper.findUsersInDatabase();
 
-  const newUser = {
-    username: "ho0988",
-    name: "2 testing new Name",
-    password: "abcdefg",
-  };
+    const newUser = {
+      username: "ho0988",
+      name: "2 testing new Name",
+      password: "abcdefg",
+    };
 
-  await api
-    .post("/api/users")
-    .send(newUser)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const usersAfter = await helper.findUsersInDatabase();
-  expect(usersAfter).toHaveLength(usersBefore.length + 1);
-});
+    const usersAfter = await helper.findUsersInDatabase();
+    expect(usersAfter).toHaveLength(usersBefore.length + 1);
+  });
 
-test("Attempts to create a user with an already existing username will yield a 400 status", async () => {
-  const usersBefore = await helper.findUsersInDatabase();
+  test("Attempts to create a user with an already existing username will yield a 400 status", async () => {
+    const usersBefore = await helper.findUsersInDatabase();
 
-  const newUser = usersBefore[0];
+    const newUser = {
+      username: "testing 1111111",
+      name: "2 testing new Name",
+      password: "abcdefg",
+    };
 
-  await api.post("/api/users").send(newUser).expect(400);
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
 
-  const usersAfter = await helper.findUsersInDatabase();
-  expect(usersAfter).toHaveLength(usersBefore.length);
-});
+    expect(result.body.error).toContain("expected `username` to be unique");
+
+    const usersAfter = await helper.findUsersInDatabase();
+    expect(usersAfter).toHaveLength(usersBefore.length);
+  });
+}, 15000);
 
 afterAll(async () => {
   await mongoose.connection.close();

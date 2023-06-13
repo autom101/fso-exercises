55093936a3b3fc25522bbed3889f8ef6e5e591d6
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-06-12 19:25:01 -0600

    finish ex 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false);
+  const [likeCount, setLikeCount] = useState(blog.likes);
+
+  const toggleVisibility = () => {
+    setShowAll(!showAll);
+  };
+
+  const infoShown = () => {
+    if (showAll) {
+      return (
+        <>
+          {blog.title} by {blog.author}{" "}
+          <button onClick={toggleVisibility}>Expand</button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          {blog.title} by {blog.author}{" "}
+          <button onClick={toggleVisibility}>Collapse</button>
+          <p> {blog.url}</p>
+          <p>
+            {" "}
+            likes: {likeCount}{" "}
+            <button
+              onClick={() => {
+                const newCount = likeCount + 1;
+                setLikeCount(newCount);
+              }}
+            >
+              like
+            </button>
+          </p>
+          <p> {blog.user.name}</p>
+        </>
+      );
+    }
+  };
+
+  return <div className="individual-blog">{infoShown()}</div>;
+};
 
 export default Blog;

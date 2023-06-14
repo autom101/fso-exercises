49aa40aecf1505283fda06c3593c420d5a3cd32b
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-06-14 11:26:32 -0600

    Finish ex 5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -20,20 +20,41 @@ test("title and author are defined", async () => {
   const removeBlog = jest.fn();
   const modifyLikes = jest.fn();
 
-  const user = userEvent.setup();
   render(<Blog blog={blog} remove={removeBlog} modifyLikes={modifyLikes} />);
-  screen.debug();
 
   const blogTitle = screen.getByText("a random title", { exact: false });
-  const blogAuthor = screen.getByText("a random title", { exact: false });
-
-  await user.click();
+  const blogAuthor = screen.getByText("some random author", { exact: false });
 
   expect(blogTitle).toBeDefined();
   expect(blogAuthor).toBeDefined();
 });
 
-test("url and likes are not defined", async () => {
+test("url and likes are not initially defined", async () => {
+  const blog = {
+    title: "a random title",
+    author: "some random author",
+    user: {
+      name: "name",
+      id: "912903123sla;jc",
+    },
+    id: "0293ufjalskdf023f",
+    likes: 7,
+    url: "url",
+  };
+
+  const removeBlog = jest.fn();
+  const modifyLikes = jest.fn();
+
+  render(<Blog blog={blog} remove={removeBlog} modifyLikes={modifyLikes} />);
+
+  const blogLikes = screen.queryByText("7", { equals: false });
+  const blogUrl = screen.queryByText("url", { equals: false });
+
+  expect(blogLikes).toBeNull();
+  expect(blogUrl).toBeNull();
+});
+
+test("url and likes are not defined after the expand button is clicked", async () => {
   const blog = {
     title: "a random title",
     author: "some random author",
@@ -52,11 +73,15 @@ test("url and likes are not defined", async () => {
   const user = userEvent.setup();
   render(<Blog blog={blog} remove={removeBlog} modifyLikes={modifyLikes} />);
 
-  const blogLikes = screen.queryByText("a random title", { equals: false });
-  const blogUrl = screen.queryByText("a random title", { equals: false });
-
-  await user.click();
+  const blogLikes = screen.queryByText("7", { equals: false });
+  const blogUrl = screen.queryByText("url", { equals: false });
 
   expect(blogLikes).toBeNull();
   expect(blogUrl).toBeNull();
+
+  const showButton = screen.queryByText("Expand");
+  await user.click(showButton);
+
+  expect(blogLikes).toBeDefined();
+  expect(blogUrl).toBeDefined();
 });

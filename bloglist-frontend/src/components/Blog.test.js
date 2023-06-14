9b49809fb6d9f51901859d258cc42df219bb66fd
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-06-14 11:32:56 -0600

    Finish ex 5.15

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -4,6 +4,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
+let modifyLikes;
 beforeEach(() => {
   const blog = {
     title: "a random title",
@@ -18,7 +19,7 @@ beforeEach(() => {
   };
 
   const removeBlog = jest.fn();
-  const modifyLikes = jest.fn();
+  modifyLikes = jest.fn();
 
   render(<Blog blog={blog} remove={removeBlog} modifyLikes={modifyLikes} />);
 });
@@ -53,4 +54,26 @@ test("url and likes are defined after the expand button is clicked", async () =>
 
   expect(blogLikes).toBeDefined();
   expect(blogUrl).toBeDefined();
+});
+
+test("modifyLikes function is called when like button is clicked", async () => {
+  const user = userEvent.setup();
+
+  const blogLikes = screen.queryByText("7", { equals: false });
+  const blogUrl = screen.queryByText("url", { equals: false });
+
+  expect(blogLikes).toBeNull();
+  expect(blogUrl).toBeNull();
+
+  const showButton = screen.queryByText("Expand", { equals: false });
+  await user.click(showButton);
+
+  expect(blogLikes).toBeDefined();
+  expect(blogUrl).toBeDefined();
+
+  const likeButton = screen.queryByText("like", { equals: false });
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  expect(modifyLikes.mock.calls).toHaveLength(2);
 });

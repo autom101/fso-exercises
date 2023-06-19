b506c6b8f65e01b427c5716443b25d4f4bee46ee
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-06-19 10:11:29 -0600

    Finish ex 6.12

---
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: "message",
+  initialState: "Initial message",
+  reducers: {
+    //
+  },
+});
+
+export default notificationSlice.reducer;
